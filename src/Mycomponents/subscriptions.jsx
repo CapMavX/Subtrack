48dea3009e05aeba961c89aa23031b3ad0adcbf9
CapMavX <abubakarsadiqm450@gmail.com>
2025-01-15 12:36:47 -0800

    useful

--- src/Mycomponents/subscriptions.jsx
+++ src/Mycomponents/subscriptions.jsx
@@ -5,11 +5,8 @@ export default function Subscriptions() {
             <div className='flex flex-col mt-[30px]'>
 
   <h4 className='w-[197px] h-[24px] mt-[15px] font-[Montserrat] font-[700] text-[20px] leading-[24.38px]'>Your Subscriptions</h4>
-  <div className='w-[500px] h-[263px] rounded-[16px] gap-[12px] flex flex-col bg-[black] items-center'>
- <div className='h-[15px] w-[450px] flex flex-row bg-[white] mt-[7px]'>
-      <p className='w-[37px] h-[15px] font-[Montserrat] font-[400] text-[12px] leading-[14.63px] '>Name</p>
-      <p className='w-[]'></p>
- </div>
+  <div className='w-[500px] h-[263px] rounded-[16px] gap-[12px] flex flex-col items-center'>
+
 
  
 </div>
